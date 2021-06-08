5c4d7ea101fe55c4484814aaaa170f5d3bd7bc56
Haru39M <s2071149@s.konan-u.ac.jp>
2021-06-08 21:55:03 +0900

    音声がルーム外で再生される問題を修正

--- app/src/main/java/app/wakayama/harusame/karaoke/playRoomActivity.kt
+++ app/src/main/java/app/wakayama/harusame/karaoke/playRoomActivity.kt
@@ -16,10 +16,22 @@ import kotlinx.android.synthetic.main.activity_play_room.*
 // Access a Cloud Firestore instance from your Activity
 val db = Firebase.firestore
 
+
 class playRoomActivity : AppCompatActivity() {
+    //複数のoverride内で使えるようにとりあえず宣言
+    lateinit var Sound1:MediaPlayer
+    lateinit var Sound2:MediaPlayer
+    lateinit var Sound3:MediaPlayer
+    lateinit var Sound4:MediaPlayer
+    lateinit var Sound5:MediaPlayer
+    lateinit var Sound6:MediaPlayer
+    lateinit var Sound7:MediaPlayer
+    lateinit var Sound8:MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_room)
+
 
         //joinで選択されたルーム名を取得
         val selectedRoom: String = intent.getStringExtra("selectedRoom").toString()
@@ -36,6 +48,12 @@ class playRoomActivity : AppCompatActivity() {
         fun pushMusic(roomId:String,musicId:String){
             db.collection(collectionName).document(roomId)
                 .update(musicId,FieldValue.increment(1))
+                .addOnSuccessListener {
+                    Log.d("TAG","再生リストを+1しました。")
+                }
+                .addOnFailureListener {
+                    Log.d("TAG","再生リストの更新に失敗しました。")
+                }
         }
 
         //受信側。更新があると音楽を再生。
@@ -56,92 +74,60 @@ class playRoomActivity : AppCompatActivity() {
                         Log.d("TAG", "receive played!!!")
                         when(soundId){
                             "sound1" -> {
-                                val drumSound = MediaPlayer.create(this,R.raw.hyoushigi)//Rはresのこと。.でパスを表している
-                                drumSound.seekTo(0)
-                                drumSound.start()
+                                Sound1.seekTo(0)
+                                Sound1.start()
                                 //データを更新
                                 playList -= 1
-                                val newData = hashMapOf(
-                                    soundId to playList
-                                )
-                                docRef.set(newData, SetOptions.merge())//データを統合
+                                docRef.update(soundId,playList)
                             }
                             "sound2" -> {
-                                val drumSound = MediaPlayer.create(this,R.raw.hyoushigi2)//Rはresのこと。.でパスを表している
-                                drumSound.seekTo(0)
-                                drumSound.start()
+                                Sound2.seekTo(0)
+                                Sound2.start()
                                 //データを更新
                                 playList -= 1
-                                val newData = hashMapOf(
-                                    soundId to playList
-                                )
-                                docRef.set(newData, SetOptions.merge())//データを統合
+                                docRef.update(soundId,playList)
                             }
                             "sound3" -> {
-                                val drumSound = MediaPlayer.create(this,R.raw.wadaiko)//Rはresのこと。.でパスを表している
-                                drumSound.seekTo(0)
-                                drumSound.start()
+                                Sound3.seekTo(0)
+                                Sound3.start()
                                 //データを更新
                                 playList -= 1
-                                val newData = hashMapOf(
-                                    soundId to playList
-                                )
-                                docRef.set(newData, SetOptions.merge())//データを統合
+                                docRef.update(soundId,playList)
                             }
                             "sound4" -> {
-                                val drumSound = MediaPlayer.create(this,R.raw.wadaiko2)//Rはresのこと。.でパスを表している
-                                drumSound.seekTo(0)
-                                drumSound.start()
+                                Sound4.seekTo(0)
+                                Sound4.start()
                                 //データを更新
                                 playList -= 1
-                                val newData = hashMapOf(
-                                    soundId to playList
-                                )
-                                docRef.set(newData, SetOptions.merge())//データを統合
+                                docRef.update(soundId,playList)
                             }
                             "sound5" -> {
-                                val drumSound = MediaPlayer.create(this,R.raw.dora)//Rはresのこと。.でパスを表している
-                                drumSound.seekTo(0)
-                                drumSound.start()
+                                Sound5.seekTo(0)
+                                Sound5.start()
                                 //データを更新
                                 playList -= 1
-                                val newData = hashMapOf(
-                                    soundId to playList
-                                )
-                                docRef.set(newData, SetOptions.merge())//データを統合
+                                docRef.update(soundId,playList)
                             }
                             "sound6" -> {
-                                val drumSound = MediaPlayer.create(this,R.raw.castanet)//Rはresのこと。.でパスを表している
-                                drumSound.seekTo(0)
-                                drumSound.start()
+                                Sound6.seekTo(0)
+                                Sound6.start()
                                 //データを更新
                                 playList -= 1
-                                val newData = hashMapOf(
-                                    soundId to playList
-                                )
-                                docRef.set(newData, SetOptions.merge())//データを統合
+                                docRef.update(soundId,playList)
                             }
                             "sound7" -> {
-                                val drumSound = MediaPlayer.create(this,R.raw.kozutsumi)//Rはresのこと。.でパスを表している
-                                drumSound.seekTo(0)
-                                drumSound.start()
+                                Sound7.seekTo(0)
+                                Sound7.start()
                                 //データを更新
                                 playList -= 1
-                                val newData = hashMapOf(
-                                    soundId to playList
-                                )
-                                docRef.set(newData, SetOptions.merge())//データを統合
+                                docRef.update(soundId,playList)
                             }
                             "sound8" -> {
-                                val drumSound = MediaPlayer.create(this,R.raw.oh_man)//Rはresのこと。.でパスを表している
-                                drumSound.seekTo(0)
-                                drumSound.start()
+                                Sound8.seekTo(0)
+                                Sound8.start()
                                 //データを更新
                                 playList -= 1
-                                val newData = hashMapOf(
-                                    soundId to playList
-                                )
-                                docRef.set(newData, SetOptions.merge())//データを統合
+                                docRef.update(soundId,playList)
                             }
                         }
                     }
@@ -198,6 +184,16 @@ class playRoomActivity : AppCompatActivity() {
     //入室処理
     override fun onResume() {
         super.onResume()
+        //音声を再生できるようにインスタンスを作成
+        Sound1 = MediaPlayer.create(this,R.raw.hyoushigi)//Rはresのこと。.でパスを表している
+        Sound2 = MediaPlayer.create(this,R.raw.hyoushigi2)//Rはresのこと。.でパスを表している
+        Sound3 = MediaPlayer.create(this,R.raw.wadaiko)//Rはresのこと。.でパスを表している
+        Sound4 = MediaPlayer.create(this,R.raw.wadaiko2)//Rはresのこと。.でパスを表している
+        Sound5 = MediaPlayer.create(this,R.raw.dora)//Rはresのこと。.でパスを表している
+        Sound6 = MediaPlayer.create(this,R.raw.castanet)//Rはresのこと。.でパスを表している
+        Sound7 = MediaPlayer.create(this,R.raw.kozutsumi)//Rはresのこと。.でパスを表している
+        Sound8 = MediaPlayer.create(this,R.raw.oh_man)//Rはresのこと。.でパスを表している
+
         //joinで選択されたルーム名を取得
         val selectedRoom: String = intent.getStringExtra("selectedRoom").toString()
         Log.d("TAG","get roomID => ${selectedRoom}")
@@ -236,5 +232,14 @@ class playRoomActivity : AppCompatActivity() {
             .addOnFailureListener {
                 Log.d("TAG","入室人数の更新(退室)に失敗しました。")
             }
+        //音声インスタンスを破棄
+        Sound1.stop()
+        Sound2.stop()
+        Sound3.stop()
+        Sound4.stop()
+        Sound5.stop()
+        Sound6.stop()
+        Sound7.stop()
+        Sound8.stop()
     }
 }
